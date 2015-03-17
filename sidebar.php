945fcd1933f99cbafe15383f3d17679795b1eaf9
Pea <pea@misfist.com>
2015-03-17 02:20:25 -0400

    Grids and other styling.

--- sidebar.php
+++ sidebar.php
@@ -10,6 +10,14 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
+
 <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
+
+<script>
+    // Add class to widget that will distinquish between testimonials and quotes
+    document.querySelector('div.category-testimonial').parentElement.className += ' type-testimonial';
+    document.querySelector('div.category-quote').parentElement.className += ' type-quote';
+</script>
+
