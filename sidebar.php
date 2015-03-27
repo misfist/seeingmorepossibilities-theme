3d975bdf8f7b8edc5a028a1255e0a3c364efa3f8
Pea <pea@misfist.com>
2015-03-27 01:12:33 -0400

    Script to add testimonial type class to widget.

--- sidebar.php
+++ sidebar.php
@@ -15,10 +15,3 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	<?php dynamic_sidebar( 'social-links' ); ?>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
-
-<script>
-    // Add class to widget that will distinquish between testimonials and quotes
-    document.querySelector('div.category-testimonial').parentElement.className += ' type-testimonial';
-    document.querySelector('div.category-quote').parentElement.className += ' type-quote';
-</script>
-
