18160371980aff7b38ae322acf463831c70403e4
Pea <pea@misfist.com>
2015-04-15 13:24:11 -0400

    Coming soon page and styles.

--- page-coming-soon.php
+++ page-coming-soon.php
@@ -1,38 +1,24 @@
 <?php
 /**
- * The template for displaying all pages.
+ * Template Name: Coming Soon
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
  *
  * @package Seeing More Possibilities
  */
 
-get_header(); ?>
+wp_head(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area coming-soon">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+			<?php the_content(); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+		<?php endwhile; // end of the loop. ?>
 
-			<?php endwhile; // end of the loop. ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<?php wp_footer(); ?>
 
-<?php// get_sidebar(); ?>
-<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'social-links' ); ?>
-</div><!-- #secondary -->
-<?php get_footer(); ?>
